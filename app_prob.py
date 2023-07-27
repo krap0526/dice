0961ea75bd003c60df69b3eb3364df51ff35d916
krap0526 <140075010+krap0526@users.noreply.github.com>
2023-07-27 11:12:15 +0900

    Update app_prob.py

--- app_prob.py
+++ app_prob.py
@@ -17,9 +17,12 @@ if trial:
 
 prob = []
 print(sum(state))
-for i in state:
-    prob.append(i/sum(state))
-print(prob)
+try:
+    for i in state:
+        prob.append(i/sum(state))
+except:
+    pass
+
 
 index = [1,2,3,4,5,6]
 table = pd.DataFrame({"횟수": state, "확률값": prob}, index=index)
